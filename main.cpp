c5cbc47fc898746e6ba9e9412f983b6c65e37587
Sujal <sujalgyawali4@gmail.com>
2025-04-29 07:11:33 +0545

    Organizing into class

--- main.cpp
+++ main.cpp
@@ -23,24 +23,32 @@ class Scanner{
     }
 };
 
-void run(string& source){
-    Scanner scanner(source);
-    vector<Token> token = scanner.scantokens();
-    for(token& token:tokens)
-    {
-        cout<<token.toString()<<endl;
-    }
-    if(had_error){
-        exit(65);
-    }
 
+
+
+class Journey{
+    private:
+    string source;
+    bool had_error;
+    public:
+int main(int argc,char* argv[] ){
+   if(argc>2){
+    cout<<"Usage journey [script]"<<endl;
+    return 64;
+   }
+   else if(argc ==2)
+   {
+    runFile(argv[1]);
+   }
+   else{
+    runPrompt();
+   }
 }
-
-void runFile(const string& path){
+   void runFile(const string& path){
     ifstream file(path, ios::in | ios::binary);
     stringstream buffer;
     buffer << file.rdbuf();
-    string filecontent = buffer.str()
+    string filecontent = buffer.str();
     run(filecontent);
 
 }
@@ -56,19 +64,17 @@ void runPrompt(){
        had_error = false;
     }
 }
-
-int main(int argc,char* argv[] ){
-   if(argc>2){
-    cout<<"Usage journey [script]"<<endl;
-    return 64;
-   }
-   else if(argc ==2)
-   {
-    runFile(argv[1]);
-   }
-   else{
-    runPrompt();
-   }
-
+void run(string& source){
+    Scanner scanner(source);
+    vector<Token> token = scanner.scantokens();
+    for(token& token:tokens)
+    {
+        cout<<token.toString()<<endl;
+    }
+    if(had_error){
+        exit(65);
+    }
 
 }
+
+};
